3a75ae583b172fdedba6c5a3c5a8f1e228d3e7f6
SivaBhargaviPesarlanka <123106688+SivaBhargaviPesarlanka@users.noreply.github.com>
2023-03-29 11:10:38 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./index');
-var usersRouter = require('./users');
-var mydataRouter = require('./mydata');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
